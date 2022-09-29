68e58acd26862624e385a9ccef165a2fa511fe9e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-29 23:07:50 +0200

    spaces?

--- .github/actions/prisma-transformer/index.mjs
+++ .github/actions/prisma-transformer/index.mjs
@@ -9019,17 +9019,21 @@ async function run() {
   const prismaPath = path__WEBPACK_IMPORTED_MODULE_3__.join(cwd, PRISMA_PATH);
   const prisma = fs__WEBPACK_IMPORTED_MODULE_2__.readFileSync(prismaPath, "utf8");
 
+  // Remove spaces
+  const lines = prisma.split("\n").map((l) => l.trim());
+  console.log(lines);
+
   // Find datasource block
   const datasource = prisma.match(/datasource db \{[^}]*\}/g)[0];
   console.log(datasource);
 
   // Replace provider
-  const provider = datasource.match(/provider = "[^"]*"/g)[0];
+  const provider = datasource.match(/provider( +)=( +)"[^"]*"/g)[0];
   console.log(provider);
   const newProvider = provider.replace("sqlite", "postgresql");
 
   // Replace url
-  const url = datasource.match(/url = "[^"]*"/g)[0];
+  const url = datasource.match(/url( +)=( +)"[^"]*"/g)[0];
   console.log(url);
   const newUrl = url.replace("postgres", "sqlite");
 
--- .github/actions/prisma-transformer/src/index.mjs
+++ .github/actions/prisma-transformer/src/index.mjs
@@ -10,17 +10,21 @@ async function run() {
   const prismaPath = path.join(cwd, PRISMA_PATH);
   const prisma = fs.readFileSync(prismaPath, "utf8");
 
+  // Remove spaces
+  const lines = prisma.split("\n").map((l) => l.trim());
+  console.log(lines);
+
   // Find datasource block
   const datasource = prisma.match(/datasource db \{[^}]*\}/g)[0];
   console.log(datasource);
 
   // Replace provider
-  const provider = datasource.match(/provider = "[^"]*"/g)[0];
+  const provider = datasource.match(/provider( +)=( +)"[^"]*"/g)[0];
   console.log(provider);
   const newProvider = provider.replace("sqlite", "postgresql");
 
   // Replace url
-  const url = datasource.match(/url = "[^"]*"/g)[0];
+  const url = datasource.match(/url( +)=( +)"[^"]*"/g)[0];
   console.log(url);
   const newUrl = url.replace("postgres", "sqlite");
 
