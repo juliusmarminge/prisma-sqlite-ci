7a9bdc86f64c4904bfa4e15451c13597c6631997
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-29 23:02:19 +0200

    copilot rocks

--- .github/actions/prisma-transformer/index.mjs
+++ .github/actions/prisma-transformer/index.mjs
@@ -9022,6 +9022,28 @@ async function run() {
   // Find datasource block
   const datasource = prisma.match(/datasource db \{[^}]*\}/g)[0];
   console.log(datasource);
+
+  // Replace provider
+  const provider = datasource.match(/provider = "[^"]*"/g)[0];
+  console.log(provider);
+  const newProvider = provider.replace("sqlite", "postgresql");
+
+  // Replace url
+  const url = datasource.match(/url = "[^"]*"/g)[0];
+  console.log(url);
+  const newUrl = url.replace("postgres", "sqlite");
+
+  // Replace datasource block
+  const newDatasource = datasource
+    .replace(provider, newProvider)
+    .replace(url, newUrl);
+
+  // Replace prisma file
+  const newPrisma = prisma.replace(datasource, newDatasource);
+  fs__WEBPACK_IMPORTED_MODULE_2__.writeFileSync(prismaPath, newPrisma);
+
+  console.log("Prisma file updated");
+  console.log(newPrisma);
 }
 
 run();
