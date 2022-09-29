d5845c08d63e5fb3912537fd4d85dde217d8e265
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-29 22:47:09 +0200

    init

---
+++ .github/actions/prisma-transformer/index.mjs
@@ -0,0 +1,26 @@
+import github from "@actions/github";
+import core from "@actions/core";
+
+import path from "path";
+
+async function run() {
+  const octokit = github.getOctokit(myToken);
+  const myToken = core.getInput("GITHUB_TOKEN");
+
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner: "octokit",
+    repo: "rest.js",
+    pull_number: 123,
+    mediaType: {
+      format: "diff",
+    },
+  });
+
+  console.log(pullRequest);
+
+  console.log("Current path", path.resolve());
+  console.log("Current directory", process.cwd());
+  console.log("Directory contents", fs.readdirSync(process.cwd()));
+}
+
+run();
